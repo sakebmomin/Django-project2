7966193bcd0d5f563d334a664101d52c0b39ca61
Sakeb Momin <82925107+sakebmomin@users.noreply.github.com>
2022-06-22 11:59:04 +0530

    Allowed Hosts

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -13,10 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from ntpath import join
 import os
 from pathlib import Path
-from tkinter import *
 
-Label(text='Spam').pack()
-mainloop()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
